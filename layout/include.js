f7ae924f3ecc184bf7650138a7f807a0f11f1c0d
AntonyCollin <72162477+AntonyCollin@users.noreply.github.com>
2023-06-09 22:03:42 -0400

    init

---
+++ layout/include.js
@@ -0,0 +1,14 @@
+// $(function () {
+//   $("header").load("../layout/header.html");
+//   $("footer").load("../layout/footer.html");
+// });
+
+// $(document).ready(function () {
+//   console.log("test");
+//   $("href").each(function () {
+//     console.log("oui");
+//     if (this.href == window.location.href) {
+//       $(this).addClass("active");
+//     }
+//   });
+// });
